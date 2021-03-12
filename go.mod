476a239af57007d3d7bc9da0160dc62fbbdb6751
oreyes <oreyes@grupotecun.com>
2021-03-12 11:03:42 -0600

    :hammer: first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/areyesoleon/go-tree-pay-core
+
+go 1.16
+
+require go.mongodb.org/mongo-driver v1.5.0 // indirect
---
+++ model.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Res struct {
+	Ok      bool        `json:"ok"`
+	Res     interface{} `json:"res,omitempty"`
+	Message string      `json:"message,omitempty"`
+}
+
+// TimeData ...
+type TimeData struct {
+	Create time.Time `json:"create"`
+	Update time.Time `json:"update"`
+}
+
+//User ...
+type User struct {
+	ID       primitive.ObjectID `bson:"_id, omitempty" json:"id,omitempty"`
+	Name     string             `json:"name"`
+	LastName string             `json:"lastName"`
+	Email    string             `json:"email"`
+	Password string             `json:"password"`
+	Admin    bool               `json:"admin"`
+	TimeData
+}
